1726cc1c410225f97cba3bef3da4b59963dad067
amani momoh <mahneebae@gamil.com>
2022-06-25 18:31:36 +0100

    First commit, adding all neccessary files required for the task

---
+++ file.js
@@ -0,0 +1,28 @@
+const operator = window.prompt("Enter chioce: +, -, /, *, %");
+const num1 = parseFloat(prompt("Enter first number:  "));
+const num2 = parseFloat(prompt("Enter second number"));
+
+
+let result;
+
+if (operator == '+') {
+    result = num1 + num2;
+
+
+} else if (operator == '-') {
+    result = num1 - num2;
+
+} else if (operator == '/') {
+    result = num1 / num2;
+
+
+} else if (operator == '*') {
+    reult = num1 * num2;
+
+
+} else {
+    result = num1 % num2;
+
+}
+
+alert(result);
